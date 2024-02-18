07bb58ea37ca5f0e02a1bda0acb6a906378f440b
shandil663 <sahilshandil2@gmail.com>
2024-02-18 11:43:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/farmerkonnect/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.farmerkonnect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.onbd);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Onboardpage.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/farmerkonnect/Onboardpage.java
@@ -0,0 +1,14 @@
+package com.example.farmerkonnect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Onboardpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboardpage);
+    }
+}
