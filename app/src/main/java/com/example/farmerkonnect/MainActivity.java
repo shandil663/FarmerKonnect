006c7752ba870dc08d298a1db09054f2cbc2cb9b
shandil663 <sahilshandil2@gmail.com>
2024-02-18 13:10:42 +0530

    Update1

--- app/src/main/java/com/example/farmerkonnect/MainActivity.java
+++ app/src/main/java/com/example/farmerkonnect/MainActivity.java
@@ -6,15 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 Button btn;
+TextView sign;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn=findViewById(R.id.onbd);
+        sign=findViewById(R.id.intext);
+        sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), LoginPage.class));
+            }
+        });
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/farmerkonnect/Onboardpage.java
+++ app/src/main/java/com/example/farmerkonnect/Onboardpage.java
@@ -2,13 +2,23 @@ package com.example.farmerkonnect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Onboardpage extends AppCompatActivity {
-
+TextView sign;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboardpage);
+        sign=findViewById(R.id.signinbtn);
+        sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), LoginPage.class));
+            }
+        });
     }
 }
